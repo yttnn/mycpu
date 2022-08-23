17b45e2d0fd9bda71b3b739ee88a2a3406f9772a
yttnn <67893139+yttnn@users.noreply.github.com>
2022-08-23 15:29:00 +0900

    first commit

---
+++ chisel-template/src/main/scala/02_decode/Core.scala
@@ -0,0 +1,52 @@
+package decode
+
+import chisel3._
+import common.Consts._
+
+class Core extends Module {
+  val io = IO(new Bundle {
+    val imem = Flipped(new ImemPortIo())
+    val exit = Output(Bool())
+  })
+
+  // generate 32bit x 32 register
+  // WORD_LEN = 32 defined in Consts.scala
+  val regfile = Mem(32, UInt(WORD_LEN.W))
+
+  //**************************
+  // Instruction Fetch Stage
+  // Load instructions from Memory
+
+  // generate PC register initialized 0
+  val pc_reg = RegInit(START_ADDR)
+  pc_reg := pc_reg + 4.U(WORD_LEN.W)
+
+  // connect pc_reg to output_port_addr
+  io.imem.addr := pc_reg
+  val inst = io.imem.inst
+
+
+  //*************************
+  // Instruction Decode Stage
+  // p25       (wb <-> rd ?)
+  // get register_number
+  val rs1_addr = inst(19, 15)
+  val rs2_addr = inst(24, 20)
+  val wb_addr = inst(11, 7)
+  // read register_data,    Mux: Multiplexer
+  val rs1_data = Mux((rs1_addr =/= 0.U(WORD_LEN.U)), regfile(rs1_addr), 0.U(WORD_LEN.W))
+  val rs2_data = Mux((rs2_addr =/= 0.U(WORD_LEN.U)), regfile(rs2_addr), 0.U(WORD_LEN.W))
+
+
+
+  // for debug
+  io.exit := (inst === 0x34333231.U(WORD_LEN.W))
+  printf(p"pc_reg   : 0x${Hexadecimal(pc_reg)}\n")
+  printf(p"inst     : 0x${Hexadecimal(inst)}\n")
+  printf(p"rs1_addr : $rs1_addr\n")
+  printf(p"rs2_addr : $rs2_addr\n")
+  printf(p"wb_addr  : $wb_addr\n")
+  printf(p"rs1_data : 0x${Hexadecimal(rs1_data)}\n")
+  printf(p"rs2_data : 0x${Hexadecimal(rs2_data)}\n")
+  printf("---------\n")
+}
---
+++ chisel-template/src/test/scala/DecodeTest.scala
@@ -0,0 +1,16 @@
+package decode
+
+import chisel3._
+import org.scalatest._
+import org.scalatest.flatspec._
+import chiseltest._
+
+class HexTest extends AnyFlatSpec with ChiselScalatestTester {
+  "mycpu" should "work through hex" in {
+    test(new Top) { c =>
+      while(!c.io.exit.peek().litToBoolean) {
+        c.clock.step(1)
+      }
+    }
+  }
+}
