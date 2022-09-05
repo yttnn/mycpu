60059085cc9b6148e467da9cc3684b9dcf9581c1
yttnn <67893139+yttnn@users.noreply.github.com>
2022-09-05 12:22:35 +0900

    fix

--- chisel-template/src/test/scala/RiscvTests.scala
+++ chisel-template/src/test/scala/RiscvTests.scala
@@ -8,7 +8,7 @@ class RiscvTest extends FlatSpec with ChiselScalatestTester {
   behavior of "mycpu"
   it should "work through hex" in {
     test(new Top){
-      c => while(!c.io.exit.peak().litToBoolean){
+      c => while(!c.io.exit.peek().litToBoolean){
         c.clock.step(1)
       }
       c.io.gp.expect(1.U) // p154
